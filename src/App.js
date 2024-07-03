6a870286360d5cfa3661ced1b594ceb8bfa895f1
Hector Macafonty <learnwithhector@gmail.com>
2024-07-03 21:46:33 +0100

    Completed adding new fact functionality (non permanent)

--- src/App.js
+++ src/App.js
@@ -35,31 +35,21 @@ const initialFacts = [
   },
 ];
 
-// function Counter() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <span style={{ fontSize: "40px" }}>{count}</span>
-//       <button className="btn btn-large" onClick={() => setCount((c) => c + 1)}>
-//         +1
-//       </button>
-//     </div>
-//   );
-// }
-
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
       <Header showForm={showForm} setShowForm={setShowForm} />
 
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
@@ -110,7 +100,7 @@ function isValidHttpUrl(string) {
   return url.protocol === "http:" || url.protocol === "https:";
 }
 
-function NewFactForm() {
+function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
   const [source, setSource] = useState("http://example.com");
   const [category, setCategory] = useState("");
@@ -123,6 +113,7 @@ function NewFactForm() {
 
     // 2. Check if data is valid. If so, create a new fact
     if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+      // 3. Create a new fact object
       const newFact = {
         id: Math.floor(Math.random() * 10000000),
         text,
@@ -131,15 +122,17 @@ function NewFactForm() {
         votesInteresting: 0,
         votesMindblowing: 0,
         votesFalse: 0,
-        createdIn: new Date().getCurrentYear(),
+        createdIn: new Date().getFullYear(),
       };
-      // 3. Create a new fact object
 
       // 4. Add new fact to the UI: add the fact to state
-
+      setFacts((facts) => [newFact, ...facts]);
       // 5. Reset input fields
-
+      setText("");
+      setSource("");
+      setCategory("");
       // 6. Close the form
+      setShowForm(false);
     }
   }
 
@@ -193,10 +186,7 @@ function CategoryFilter() {
   );
 }
 
-function FactList() {
-  // Temporary
-  const facts = initialFacts;
-
+function FactList({ facts }) {
   return (
     <section>
       <ul className="facts-list">
